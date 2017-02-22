caed95a1877d682c856e31367873b641fb500fdc
raminrayhani <rayhani.ramin@gmail.com>
2017-02-22 14:22:10 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,41 @@
+for(var k=0; k<10; k++){
+var cntr {
+	win=0;
+	loss=0;
+},
+	function stringGen(len){
+    var text = " ";
+
+    var charset = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < len; i++ )
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+    	return text;
+
+	},
+
+	for(j=0; j<10; j++){
+		function myFunction(event) {
+	
+    	var x = document.getElementById("demo").innerHTML;
+		return x;
+	},
+        
+},
+
+	if( text === x ){
+		this.win=this.win+1:
+		document.write("Wins: "+ this.win);
+		document.write("Losses: "+ this.losses);
+		document.write("Guesses Left: "+9-j);
+	
+	} else{
+		this.loss=this.loss+1:
+		document.write("Wins: "+ this.win);
+		document.write("Losses: "+ this.losses);
+		document.write("Guesses Left: "+9-j);
+	
+	},
+};
+
+
